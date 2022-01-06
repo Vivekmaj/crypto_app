df535608c44c05fc1ecb5b612dda2b649f650c1a
Vivekmaj <59451314+Vivekmaj@users.noreply.github.com>
2022-01-06 00:19:23 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import streamlit.components.v1 as stc
+from fcast_app import run_fcast_app
+from binance_app import run_binance
+
+
+
+html_temp = """
+		<div style="background-color:tomato;padding:10px;border-radius:10px">
+		<h1 style="color:white;text-align:center;">Cryptocurrency Forecast & Binance Price App </h1>
+		</div>
+		"""
+
+def main():
+
+    stc.html(html_temp)
+    
+
+    menu = ['Home', 'Forecast Section', 'Binance Prices']
+    choice = st.sidebar.selectbox('Menu', menu)
+
+    if choice == 'Home':
+        st.write("""
+        ### App Content
+            - Forecast Section: Forecast crypto and view some basic analytics.
+            - Binance Prices: View price data for crypto pairs from Binance API.
+        """)
+        st.markdown('### Disclaimer')
+        st.write('The material on this app is purely for educational purposes and should not be taken as professional investment advice. Invest at your own discretion.')
+
+    elif choice == 'Forecast Section':
+        run_fcast_app()
+
+    elif choice == 'Binance Prices':
+        run_binance()
+
+if __name__ == '__main__':
+    main()
